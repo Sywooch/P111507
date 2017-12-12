902c515acde132a8aa9a4f923a22c721ce0ab495
KenPham <ighost190@gmail.com>
2017-12-12 23:24:19 +0700

    push config

---
+++ source/common/config/main-local.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=quickrep;port=3306',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+             // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'session' => [
+            'class' => 'yii\web\DbSession',
+
+            // Set the following if you want to use DB component other than
+            // default 'db'.
+            // 'db' => 'mydb',
+
+            // To override default session table, set the following
+            // 'sessionTable' => 'my_session',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'taichua.com@gmail.com',
+                'password' => 'ylmswqyrrbnbqcuy',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
