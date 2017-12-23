4e4ffac6037a3dae4f1c0ba412f18546146065e2
KenPham <ighost190@gmail.com>
2017-12-24 00:09:09 +0700

    //commit topics

--- source/frontend/themes/v2/views/topic/_item_view.php
+++ source/frontend/themes/v2/views/topic/_item_view.php
@@ -7,95 +7,79 @@
     $base_url = $this->theme->baseUrl;
     $total_comment = count($model->comments);
     $total_vote = count($model->votes);
-    $voted = cuser()->votedQuestion($model->id);
+   // $voted = cuser()->votedQuestion($model->id);
 ?>
-<div class="news-feed">
-    <div class="status-user-answer">
-        <div class="status-content-answer">
-            <div class="status-address">
-                <p> <?=Yii::t('frontend', 'Popular on Quora')?> </p>
+<div class="question-basic section">
+   <div class="qb-topic">
+      <p>Câu hỏi<a class="qbt-hover" href="#">Khách sạn</a></p>
+      <div class="profile">
+         <div class="profile-content">
+            <div class="profile-title">
+               <a href="#"><img src="<?=$base_url?>/images/profile-cat.png" alt=""></a>
+               <div class="profile-name"><a href="#">Khách sạn</a></div>
             </div>
-            <div class="status-title-answer">
-                <h3>
-                    <a href="<?=Url::to([$model->modulehadanswer,'slug'=>$model->slug])?>">
-                        <?=Html::encode($model->title)?>
-                    </a>
-                </h3>
+            <div class="profile-info">
+               <p>Following recent terror attacks, the mayor's office has released a 59-point plan to lure tourists back to Paris, and the city is investing millions of euros into its ...</p>
             </div>
-            <div class="status-body">
-                <div class="status-info-user-answer pt-relative">
-                    <a href="profile.php">
-                        <img 
-                        class="img-user-answer has-qtip-profile" 
-                        data-ajax="<?=$model->user->id?>" 
-                        src="<?=$model->user->getAvatar()?>">
-                    </a>
-                    <div class="user-info-answer">
-                        <a href="profile.php" class="user-name text-link">
-                            <?= Html::encode($model->user->username) ?>
-                        </a>
-                        <a href="single-post.php" class="date-answer text-link">
-                            <?= Yii::t('frontend', 'Written')?> 
-                            <?= CTimestamp::formatDate(
-                                cparams('questionCreateDateFomat'),
-                                $model->create_time
-                            )?>
-                        </a>
-                    </div>
-                </div>
-                <div class="user-description-answer">
-					<?php if(!empty($model->answer->images)):?>
-                    <div class="image-description pull-right">
-                        <img class="modal-more" src="<?=$model->answer->images?>">
-                    </div>
-					<?php endif;?>
-                    <div class="text-description-answer">
-                        <!-- limit 200 character -->
-						<p> 
-                            <?= !empty($model->answer->answers_text)
-                                ? strip_tags($model->answer->answers_text) : ''?>
-                        <a class="modal-more" href=""> (more) </a>
-                        </p>
-                    </div>   
-                </div>
-            </div>
-            <div class="status-footer border-bottom">
-                <div class="row">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 user-interactive">
-                        <!--vote-->
-                        <a href="#" 
-                        class="<?=$voted ? 'upvoted' : ''?> upvote no-icon"
-                        data-ajax="<?=$model->id?>">
-                            <span class="upvote-text"><?= Yii::t('frontend', 'Upvote') ?></span>
-                            <span class="count" data-ajax="<?=$total_vote?>"> | <?=$total_vote?> <span>
-                        </a>
-            
-                        <a href="#" 
-                            class="downvote text-link" 
-                            data-ajax="<?=$model->id?>"> 
-                            <?= Yii::t('frontend', 'Downvote') ?>
-                        </a>
-                        <!--end vote-->
-                        <a href="javascript:;" 
-                        class="comments text-link" 
-                        data-ajax="<?=$model->id?>"  
-                        id="btn_comment_<?=$model->id?>"> <?= Yii::t('frontend', 'Comments') ?> 
-                            <span 
-                            class="label label-default count" 
-                            data-ajax="<?=$total_comment?>" 
-                            id="c_q_count_<?=$model->id?>">
-                                <?=$total_comment?> + 
-                            </span>
-                        </a>
-                        <div class=" connect-social pull-right">
-                            <?= WidgetShareSocial::widget(['model' => $model]) ?>
-                            <a href="#" class="more-icon has-qtip-menu-action show_nub" data-feed-id="6">
-                                <span class="dots"></span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+         </div>
+         <div class="profile-track">
+            <div class="connect-track"><a href="#"><i class="nc-icon-outline users_add-29"></i> Theo dõi | 136k</a></div>
+            <a class="on-notice" href="#">Thêm vào yêu thích</a>
+         </div>
+      </div>
+   </div>
+   <h3 class="qb-title"><a href="#">Xin địa chỉ trung tâm luyện thi IELTS đảm bảo, uy tín ở TP.HCM?</a></h3>
+   <div class="qb-time"><span class="time-update">Cập nhật 21 giờ trước</span><a href="#">2 câu trả lời</a></div>
+   <div class="qb-action">
+      <div class="action-left">
+         <ul>
+            <li><a href="#" class="textarea-click"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Trả lời</a></li>
+            <li><a href="#">Ẩn</a></li>
+            <li class="action-report">
+               <a href="#">Báo cáo</a>
+               <div class="report-bg"></div>
+               <div class="report-popup">
+                  <div class="report-title">
+                     <h3>Báo cáo vi phạm</h3>
+                     <i class="nc-icon-outline ui-2_small-remove"></i>
+                  </div>
+                  <div class="report-content">
+                     <form action="#">
+                        <label for="r1">
+                        <input type="radio" name="report" id="r1">Nội dung này gây khó chịu hoặc không thú vị
+                        </label>
+                        <label for="r2">
+                        <input type="radio" name="report" id="r2">Tôi nghĩ nội dung này không nên xuất hiện trên Quickrep
+                        </label>
+                        <label for="r3">
+                        <input type="radio" name="report" id="r3">Quấy rối: Phân biệt hoặc đối nghịch với một người hoặc một nhóm
+                        </label>
+                        <label for="r4">
+                        <input type="radio" name="report" id="r4">Đó là nội dung spam
+                        </label>
+                     </form>
+                  </div>
+                  <div class="report-bottom">
+                     <div class="report-model">
+                        <a href="#" class="report-cancel">Hủy bỏ</a>
+                        <a href="#" class="report-send">Gửi báo cáo</a>
+                     </div>
+                  </div>
+               </div>
+            </li>
+            <li class="action-follow"><a href="#">Theo dõi <span>2</span></a></li>
+         </ul>
+      </div>
+      <div class="action-right">
+         <ul>
+            <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-share-square-o" aria-hidden="true"></i></a></li>
+         </ul>
+      </div>
+   </div>
+   <div class="qb-textarea">
+      <textarea id="froala-editor"></textarea>
+   </div>
 </div>
+<!-- .question-basic -->
